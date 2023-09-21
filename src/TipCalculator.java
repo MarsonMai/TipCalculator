38e04311bc9a5d5ffc3c45e47d2c1e22577ce745
BT_1N3_34 <sample@bths.edu>
2023-09-21 09:01:31 -0400

    added rounding

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,37 +1,46 @@
 import java.util.Scanner;
-
-
+import java.lang.Math;
 
 public class TipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int number4 = 0;
-        double number5 = 0;
+        int cost = 0;
+        double totalCost = 0;
         System.out.print("Welcome to the tip calculator! ");
         System.out.print("How many people are in the group: ");
-        int number1 = scan.nextInt();
+        int people = scan.nextInt();
         System.out.print("Whats the tip percentage? (0-100): ");
-        int number2 = scan.nextInt();
+        int tip = scan.nextInt();
         // the while loop condition is checked,
         // and if TRUE, runs the code inside.
         // when the code inside is done running, the condition is rechecked,
         // and the loop repeats as long as the condition remains TRUE.
         // when the condition becomes FALSE, it stops
-        while (number4 != -1) {
+        while (cost != -1) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
-            number4 = scan.nextInt();
-            number5 = number5 + number4;
+            cost = scan.nextInt();
+            //System.out.print("Enter the item: ");
+            //String items = scan.nextLine();
+            totalCost = totalCost + cost;
         }
-        double number6 = (number2 / 100) * (number5 + 1);
-        // code below the while loop runs after the loop ends
-        System.out.println("Total bill before tip: " + (number5 + 1));
-        System.out.println("Total percentage: " + number2 + "%");
-        System.out.println("Total Tip " + number6);
-        System.out.println("Total bill with tip: " + (number5 + 1) + number6);
-        System.out.println("Per person cost before tip: " + (number5 +1) / number1);
-        System.out.println("Tip per person: " + (number6) / number1);
-        System.out.println("Total cost per person: " + ((number5 + 1) + number6) / number1);
+        double totalPlusTip = (tip * 0.01) * (totalCost + 1);
+        double roundedTotalPlusTip = Math.round(totalPlusTip * 100.0) / 100.0;
+        double billPlusTip = totalCost + 1 + totalPlusTip;
+        double roundedBillPlusTip = Math.round(billPlusTip * 100.0) / 100.0;
+        double tipPerPerson = (totalPlusTip / people);
+        double roundedTipPerPerson = Math.round(tipPerPerson * 100.0) / 100.0;
 
+        // code below the while loop runs after the loop ends
+        System.out.println("Total bill before tip: " + (totalCost + 1)); //learned to round from https://stackoverflow.com/questions/11701399/round-up-to-2-decimal-places-in-java
+        System.out.println("Total percentage: " + tip + "%");
+        System.out.println("Total Tip " + roundedTotalPlusTip);
+        System.out.println("Total bill with tip: " + roundedBillPlusTip);
+        System.out.println("Per person cost before tip: " + (totalCost +1) / people);
+        System.out.println("Tip per person: " + roundedTipPerPerson);
+        System.out.println("Total cost per person: " + ((totalCost + 1) + totalPlusTip) / people);
+        System.out.println("------------------------------------------------");
+        //System.out.println("Items ordered: ");
+        //System.out.println();
 
 
     }
